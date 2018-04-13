1a3ed46668daa51f33318afd4d6c20cd11558307
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-04-13 16:44:18 -0700

    added new ajax code to index.html and apps.js

--- apps.js
+++ apps.js
@@ -1,15 +1,15 @@
 //Variables declaration
 var channelName = "HoiThanhTinLanhVietNam North Hollywood";
-$.get (
-    "https://www.googleapis.com/youtube/v3/playlists",{
-        part: "contentDetails",
-        forUsername: channelName,
-        key:"",
-        function (data) {
-            $.each(data.items, function () {
-                console.log(items);
-            })
-        }
-    }
-    
-)
+$(document).ready(function(){
+    $.get (
+        "https://www.googleapis.com/youtube/v3/playlists",{
+            part: "contentDetails",
+            forUsername: channelName,
+            key:"",
+            function (data) {
+                $.each(data.items, function (i, item) {
+                    console.log(item);
+                });
+            }
+        });
+});
