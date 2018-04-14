16c7b7d13d838e17c78571f9f332b0a3130ee865
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-04-13 17:04:12 -0700

    added new ajax code to index.html and apps.js

--- apps.js
+++ apps.js
@@ -2,14 +2,14 @@
 var channelName = "HoiThanhTinLanhVietNam North Hollywood";
 $(document).ready(function(){
     $.get (
-        "https://www.googleapis.com/youtube/v3/channels",{
-            part: "contentDetails",
-            forUsername: channelName,
-            key:"AIzaSyCDyE9O8JsibSiwd-D3FlNkuCDQKlYxYeo",
-            function (data) {
-                $.each(data.items, function (i, item) {
-                    console.log(item);
-                });
-            }
-        });
-});
+            "https://www.googleapis.com/youtube/v3/channels",{
+                part: "contentDetails",
+                forUsername: channelName,
+                key:"AIzaSyCDyE9O8JsibSiwd-D3FlNkuCDQKlYxYeo"},
+                function (data) {
+                    $.each(data.items, function (i, item) {
+                        console.log(item);
+                    })
+                } 
+        );
+    });
